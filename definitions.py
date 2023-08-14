bd1dd37aab1105446804f4bd86f11aec60586af1
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-08-14 09:39:03 +0800

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -4,6 +4,6 @@ definitions = {
   "PM2.5": "PM2.5 is fine particulate matter is defined as particles that are 2.5 microns or less in diameter",
   "VOC": "Volatile organic compounds (VOCs) are gases emitted from certain solids or liquids. The higher the VOC, the more concentrated they are in the environemnt.",
   "Temperature": "Temperature is how hot or cold the environment is around. The unit of measurement used here is Fahrenheit (F)",
-  "Humidity": "THIS IS HUMIDITY",
-  "Pressure": "THIS IS PRESSURE",
+  "Humidity": "Humidity is the amount of water vapor in the air. If there is a lot of water vapor in the air, the humidity will be high and vice versa",
+  "Pressure": "Differential pressure is essentially the difference in pressure between two given points.",
 }
