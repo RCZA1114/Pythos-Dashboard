6668556d2dfb19a4f359f4b2263f6422c0d5afcc
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-08-14 09:13:41 +0800

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from definitions import definitions
 
 selected_measurement = st.selectbox('Select Measurement', ('PM2.5', 'Humidity', 'Temperature', 'VOC', 'Pressure'))
+
+x = selected measurement
+
+st.write(f"This is {x}")
