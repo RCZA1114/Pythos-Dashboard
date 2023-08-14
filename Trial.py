a5f515757f1bbc02373a369c272634e461fdea5c
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-08-14 09:21:10 +0800

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -3,6 +3,8 @@ from definitions import definitions
 
 selected_measurement = st.selectbox('Select Measurement', ('PM2.5', 'Humidity', 'Temperature', 'VOC', 'Pressure'))
 
-x = selected_measurement
 
-st.write(f"This is {x}")
+for d in dictionaries:
+  if d == selected_measurement
+
+  st.write(f"This is {selected_measurement[d]}")
