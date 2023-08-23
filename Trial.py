efa62fac4229c5938045f72d68a21e190f267c30
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-08-23 11:02:49 +0800

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -55,60 +55,10 @@ current_datetime = pd.to_datetime(datetime_str)
 current_datetime = current_datetime + timedelta(hours=8)
 start_datetime = current_datetime - timedelta(hours=24)  # - 24 hours
 
-
 fifteen = current_datetime - timedelta(minutes=45)
-
 
 filtered_data = data[(data['Date/Time'] >= start_datetime) & (data['Date/Time'] <= current_datetime)]
 
-
 filtered_data_2 = data[(data['Date/Time'] >= fifteen) & (data['Date/Time'] <= current_datetime)]
-
-
-
-
-
-
-selected_measurement = st.selectbox('Select Measurement', ('PM2.5', 'Humidity', 'Temperature', 'VOC', 'Pressure'))
-st.info("")
-for d in definitions:
-    if selected_measurement==d:
-        st.info(definitions[d])
-
-tower_ids = data['Tower ID'].unique()
-
-
-selected_towers = st.multiselect('Select tower(s)', options = tower_ids, default=tower_ids)
-
-
-#___________________________________________________________________________________________________
-# Filter the data based on selected tower
-filtered_data = filtered_data[filtered_data['Tower ID'].isin(selected_towers)]
-
-
-fig = px.scatter(filtered_data, x='Date/Time', y=selected_measurement, title="Line Chart of the measurement for the last 24 hours", color='Tower ID',)
-
-
-st.plotly_chart(fig, use_container_width=True)
-
-
-#_______________________________________________________________________________________
-
-
-
-
-# Bar Chart
-
-
-filtered_data_2 = filtered_data_2[filtered_data_2['Tower ID'].isin(selected_towers)]
-fig2 = px.bar(filtered_data_2, x='Date/Time', y=selected_measurement, title="Bar Chart of the measurement for the last 45 minutes", color='Tower ID')
-
-
-#fig3 = px.line(fifteen_min, x='datetime', y=selected_measurement, title="Title", color='tower_id')
-
-
-st.plotly_chart(fig2, use_container_width=True)
-#st.plotly_chart(fig3, use_container_width=True)
-
 
 st.dataframe(filtered_data)
