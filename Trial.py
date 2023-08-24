3c165e398aa8827cafe4dec5714c4493bba4670a
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-08-24 12:58:44 +0800

    Update Trial.py

--- Trial.py
+++ Trial.py
@@ -19,29 +19,31 @@ st.sidebar.success("Select a page above.")
 
 
 st.header("Data for the Day!!!!")
-@st.cache_data
-def load_data():
+
+
+if st.button("Refresh Data"):
+
+    @st.cache_data
+    def load_data():
     #data = pd.read_csv("2023.csv", usecols=['tower_id', 'pm2_5', 'humidity', 'temp', 'voc', 'pressure', 'date', 'time'])
-    data = pd.DataFrame(sql_query, columns=['tower_id', 'pm2_5', 'humidity', 'temp', 'voc', 'pressure','last_update'])
+        data = pd.DataFrame(sql_query, columns=['tower_id', 'pm2_5', 'humidity', 'temp', 'voc', 'pressure','last_update'])
 
 
-    data['last_update'] = pd.to_datetime(data['last_update'])
+        data['last_update'] = pd.to_datetime(data['last_update'])
 
 
-    data.rename(
-        columns={"tower_id": "Tower ID", "pm2_5": "PM2.5", "voc": "VOC", "humidity": "Humidity", "temp": "Temperature", "pressure": "Pressure", "last_update": "Date/Time"},
-        inplace=True,
-    )
+        data.rename(
+            columns={"tower_id": "Tower ID", "pm2_5": "PM2.5", "voc": "VOC", "humidity": "Humidity", "temp": "Temperature", "pressure": "Pressure", "last_update": "Date/Time"},
+            inplace=True,
+        )    
 
 
-    data = data.replace({'Tower ID': {'T0703220001': 'Tower 1', 'T0703220002': 'Tower 2', 'T0703220005': 'Tower 5',
+        data = data.replace({'Tower ID': {'T0703220001': 'Tower 1', 'T0703220002': 'Tower 2', 'T0703220005': 'Tower 5',
                                       'T0703220006': 'Tower 6', 'T0703220008': 'Tower 8', 'T0703220009': 'Tower 9'}})
 
 
 
-    return data
-
-if st.button("Refresh Data"):
+        return data
     data = load_data()
     
     
