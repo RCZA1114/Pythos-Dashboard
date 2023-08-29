5654710942b3c8ca4575500f61c1bd2db1eacb4c
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-08-29 10:04:26 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,28 +1,51 @@
 
 import mysql.connector
 import pandas as pd
+import threading
 #from streamlit_autorefresh import st_autorefresh
 
 
+def get_data():
+    mydb = mysql.connector.connect(
+        host="115.147.39.104",
 
-mydb = mysql.connector.connect(
-    host="115.147.39.104",
+        user="aptadmin",
+        passwd="Pe*UyxpBZ_C0VL2c",
+        database="airlock",
+        port=5061,
+    )
+    query = "select *, DATE(last_update) as date, TIME(last_update) as time, HOUR(last_update) as hour, MINUTE(last_update) as minute, SECOND(last_update) as second, MONTH(last_update) AS month, DAY(last_update) as day, YEAR(last_update) as year from failures where last_update > '2023-01-01'"
+    sql_query = pd.read_sql(
+        "select *, DATE(last_update) as date, TIME(last_update) as time, HOUR(last_update) as hour, MINUTE(last_update) as minute, SECOND(last_update) as second, MONTH(last_update) AS month, DAY(last_update) as day, YEAR(last_update) as year from failures where last_update > '2023-01-01'",
+        mydb
+    )
+    #sql_query = pd.read_sql("select * from failures", mydb)
 
-    user="aptadmin",
-    passwd="Pe*UyxpBZ_C0VL2c",
-    database="airlock",
-    port=5061,
-)
-query = "select *, DATE(last_update) as date, TIME(last_update) as time, HOUR(last_update) as hour, MINUTE(last_update) as minute, SECOND(last_update) as second, MONTH(last_update) AS month, DAY(last_update) as day, YEAR(last_update) as year from failures where last_update > '2023-01-01'"
-sql_query = pd.read_sql(
-    "select *, DATE(last_update) as date, TIME(last_update) as time, HOUR(last_update) as hour, MINUTE(last_update) as minute, SECOND(last_update) as second, MONTH(last_update) AS month, DAY(last_update) as day, YEAR(last_update) as year from failures where last_update > '2023-01-01'",
-    mydb
-)
-#sql_query = pd.read_sql("select * from failures", mydb)
-
-df = pd.DataFrame(sql_query, columns=['tower_id', 'pm2_5', 'humidity', 'temp', 'voc', 'pressure', 'last_update', 'hour'])
-
-#df = pd.DataFrame(sql_query, columns=['last_update'])
+    data = pd.DataFrame(sql_query, columns=['tower_id', 'pm2_5', 'humidity', 'temp', 'voc', 'pressure','last_update'])
 
 
-#st_autorefresh(interval=5 * 60 * 1000, key="dataframerefresh")
+    data['last_update'] = pd.to_datetime(data['last_update'])
+
+
+    data.rename(
+        columns={"tower_id": "Tower ID", "pm2_5": "PM2.5", "voc": "VOC", "humidity": "Humidity", "temp": "Temperature", "pressure": "Pressure", "last_update": "Date/Time"},
+        inplace=True,
+    )
+
+
+    data = data.replace({'Tower ID': {'T0703220001': 'Tower 1', 'T0703220002': 'Tower 2', 'T0703220005': 'Tower 5',
+                                      'T0703220006': 'Tower 6', 'T0703220008': 'Tower 8', 'T0703220009': 'Tower 9'}})
+
+
+
+
+    p = threading.Timer(180, get_data)
+
+    p.start()
+
+
+    return data
+
+    
+
+    #st_autorefresh(interval=5 * 60 * 1000, key="dataframerefresh")
