1180fb9a633083a5f42944b2a43fdcdf60436f0f
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-10-11 14:13:36 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from datetime import timedelta
 import plotly.express as px
 from data import *
+from definitions import *
 #from read_csv import read_data
 #______________________________SETTING UP THE DATA
 
