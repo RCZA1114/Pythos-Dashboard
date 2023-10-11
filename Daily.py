40d458fa5ab7d88d51f42161bbf4eb30414fa797
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2023-10-11 13:46:31 +0800

    Update Daily.py

--- Daily.py
+++ Daily.py
@@ -1,48 +1,39 @@
 import mysql.connector
 import pandas as pd
+import threading
+import time
 import streamlit as st
-from datetime import timedelta, date, time
+from datetime import timedelta
 import plotly.express as px
 #from streamlit_autorefresh import st_autorefresh
 
 
-
-
-st.set_page_config(
-    page_title="Dashboard",
-    page_icon="👋",
-)
-
-
-st.title("Dashboard")
-
-#______________________________________________
-
-def load_data(): 
-    mydb = mysql.connector.connect(
+def get_data():
+    mydb = mysql.connector.connect(    #GEt The Data from the Database
         host="115.147.39.104",
-    
+
         user="aptadmin",
         passwd="Pe*UyxpBZ_C0VL2c",
         database="airlock",
         port=5061,
     )
-    query = "select *, DATE(last_update) as date, TIME(last_update) as time, HOUR(last_update) as hour, MINUTE(last_update) as minute, SECOND(last_update) as second, MONTH(last_update) AS month, DAY(last_update) as day, YEAR(last_update) as year from failures where last_update > '2023-01-01'"
+    #3query = "select *, DATE(last_update) as date, TIME(last_update) as time, HOUR(last_update) as hour, MINUTE(last_update) as minute, SECOND(last_update) as second, MONTH(last_update) AS month, DAY(last_update) as day, YEAR(last_update) as year from failures where last_update > '2023-01-01'"
     sql_query = pd.read_sql(
         "select *, DATE(last_update) as date, TIME(last_update) as time, HOUR(last_update) as hour, MINUTE(last_update) as minute, SECOND(last_update) as second, MONTH(last_update) AS month, DAY(last_update) as day, YEAR(last_update) as year from failures where last_update > '2023-01-01'",
         mydb
     )
-    
-    
-    data = pd.DataFrame(sql_query, columns=['tower_id', 'pm2_5', 'humidity', 'temp', 'voc', 'pressure', 'last_update', 'hour'])
-  
+    #sql_query = pd.read_sql("select * from failures", mydb)
+
+    data = pd.DataFrame(sql_query, columns=['tower_id', 'pm2_5', 'humidity', 'temp', 'voc', 'pressure','last_update'])
+
+
     data['last_update'] = pd.to_datetime(data['last_update'])
 
 
     data.rename(
         columns={"tower_id": "Tower ID", "pm2_5": "PM2.5", "voc": "VOC", "humidity": "Humidity", "temp": "Temperature", "pressure": "Pressure", "last_update": "Date/Time"},
         inplace=True,
-    )
+    )   #Update the Names
 
 
     data = data.replace({'Tower ID': {'T0703220001': 'Tower 1', 'T0703220002': 'Tower 2', 'T0703220005': 'Tower 5',
@@ -50,30 +41,67 @@ def load_data():
 
 
 
+    p = threading.Timer(300, get_data)
+
+    p.start()    #Timer to Update the Data
+
+
     return data
 
-#______________________________________________________________________
-data = load_data()
+    
+
+    #st_autorefresh(interval=5 * 60 * 1000, key="dataframerefresh")
 
 
-st.dataframe(data)
 
-date = st.date_input('Input Date')
-time = st.time_input('Input Time')
+data = get_data()
 
 
-# Filter data
-datetime_str = str(date) + ' ' + str(time)
-current_datetime = pd.to_datetime(datetime_str)
+filtered_data = data.set_index('Date/Time').last('24h').reset_index()[data.columns]
 
-
-start_datetime = current_datetime - timedelta(hours=16)  # - 24 hours
+filtered_data_2 = data.set_index('Date/Time').last('1h').reset_index()[data.columns]
 
 
 
 
 
-filtered_data = data[(data['Date/Time'] >= start_datetime)]
+selected_measurement = st.selectbox('Select Measurement', ('PM2.5', 'Humidity', 'Temperature', 'VOC', 'Pressure'))
+
+
+tower_ids = data['Tower ID'].unique()
+
+
+selected_towers = st.multiselect('Select tower(s)', options = tower_ids, default=tower_ids)
+
+
+#___________________________________________________________________________________________________
+# Filter the data based on selected tower
+filtered_data = filtered_data[filtered_data['Tower ID'].isin(selected_towers)]
+
+
+fig = px.scatter(filtered_data, x='Date/Time', y=selected_measurement, title="Line Chart of the measurement for the last 24 hours", color='Tower ID',)
+
+
+st.plotly_chart(fig, use_container_width=True)
+
+
+#_______________________________________________________________________________________
+
+
+
+
+# Bar Chart
+
+
+filtered_data_2 = filtered_data_2[filtered_data_2['Tower ID'].isin(selected_towers)]
+fig2 = px.bar(filtered_data_2, x='Date/Time', y=selected_measurement, title="Bar Chart of the measurement for the last 15 minutes", color='Tower ID')
+
+
+#fig3 = px.line(fifteen_min, x='datetime', y=selected_measurement, title="Title", color='tower_id')
+
+
+st.plotly_chart(fig2, use_container_width=True)
+#st.plotly_chart(fig3, use_container_width=True)
 
 
 st.dataframe(filtered_data)
